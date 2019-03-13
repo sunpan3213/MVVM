c45fdfedb16d014d2b78e0b0c235f214c09a93a8
sunpan3213 <1046633430@qq.com>
2019-03-13 17:24:26 +0800

    日志的加入,不打印?

--- app/src/main/java/com/zkxl/www/mvvm/model/RetrofitUtils.kt
+++ app/src/main/java/com/zkxl/www/mvvm/model/RetrofitUtils.kt
@@ -13,6 +13,10 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.function.Consumer
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
 
 /**
  * Create by Panda on 2019/1/15
@@ -22,6 +26,19 @@ object RetrofitUtils {
     lateinit var apis: Apis
 
     fun init(): Apis {
+
+        val builder = OkHttpClient.Builder()
+        val loggingInterceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.NONE
+        }
+        builder.addInterceptor(loggingInterceptor)
+                .connectTimeout(15, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+
         val retrofit = Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
@@ -31,6 +48,7 @@ object RetrofitUtils {
                         } else {//发行版
                             "http://www.wanandroid.com"
                         })
+                .client(builder.build())
                 .build()
         apis = retrofit.create(Apis::class.java)
         return apis
--- app/src/main/java/com/zkxl/www/mvvm/model/bean/StateBean.kt
+++ app/src/main/java/com/zkxl/www/mvvm/model/bean/StateBean.kt
@@ -9,11 +9,14 @@ data class StateBean<T>(val errorCode: Int, val data: T?, val errorMsg: String) 
     companion object {
         const val SUCCESS = 0
         const val ERROR = 1
-        const val LOADING = 2
+        const val EXCEPTION = 2
+        const val LOADING = 3
 
         fun <T> success(data: T?) = StateBean(SUCCESS, data, "success")
 
         fun <T> error(msg: String, data: T?) = StateBean(ERROR, data, msg)
+
+        fun <T> exception(msg: String, data: T?) = StateBean(ERROR, data, msg)
 
         fun <T> loading(data: T?) = StateBean(LOADING, data, "loading")
     }
